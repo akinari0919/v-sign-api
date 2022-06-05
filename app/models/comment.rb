1f5ad6ed7787f744ef07fbd649df55d620c66ade
akinari <a.mizo0919@gmail.com>
2022-06-05 23:58:09 +0900

    add:annotateの反映

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  sign_id    :bigint           not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_comments_on_sign_id  (sign_id)
+#  index_comments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (sign_id => signs.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Comment < ApplicationRecord
   belongs_to :sign
   belongs_to :user
--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: signs
+#
+#  id         :bigint           not null, primary key
+#  angle      :float            not null
+#  image      :string           not null
+#  name       :string           not null
+#  type       :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid
+#
+# Indexes
+#
+#  index_signs_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Sign < ApplicationRecord
   belongs_to :user, optional: true # nullを許容
   has_many :comments, dependent: :destroy
