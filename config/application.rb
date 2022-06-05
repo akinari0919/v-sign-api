53db40098511916d81713e742d6948b350ec0b36
akinari <a.mizo0919@gmail.com>
2022-06-05 23:54:33 +0900

    add:ジェネレイターの設定

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,10 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.test_frameword false
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
